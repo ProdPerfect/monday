2b8ac3b3de7514e068ed447256f55fe6a0182d01
Lemuel Boyce <lemuelboyce@gmail.com>
2019-06-14 17:18:27 -0400

    handle responses

--- workstreams/resources/base.py
+++ workstreams/resources/base.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from workstreams import __version__
 from workstreams.exceptions import WorkstreamsError
@@ -14,7 +15,8 @@ class BaseResource:
         self._token = token
         self._headers = {
             'Authorization': f'Bearer {self._token}',
-            'User-Agent': f'Workstreams Python {__version__}'
+            'User-Agent': f'Workstreams Python {__version__}',
+            'Content-Type': 'application/json'
         }
         self._base_url = _URLS['prod'] if production else _URLS['dev']
         self._base_url_batch = f'{self._base_url}/batch'
@@ -23,9 +25,12 @@ class BaseResource:
         headers = self._headers
         if ws_user_id:
             headers['ws-user-id'] = ws_user_id
+
+        if data:
+            data = json.dumps(data)
         response = requests.request(method=method, url=url, data=data,
                                     params=params, headers=headers)
 
-        if response.status_code >= 200:
+        if response.status_code in (200, 201, 204):
             return response
         raise WorkstreamsError(response.content)
