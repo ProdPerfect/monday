72ee8408681bdb2c10c0ba396d8b08c239dd64d0
Lemuel Boyce <lemuelboyce@gmail.com>
2019-06-14 14:59:39 -0400

    update .gitignore and setup

--- setup.py
+++ setup.py
@@ -23,5 +23,7 @@ setup(name='workstreams',
       ],
       classifiers=[
           "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
           "Operating System :: OS Independent",
       ])
