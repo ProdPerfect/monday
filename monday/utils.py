fe54258fdc0c80c22ee6a2fc12887d1432025864
Christina D'Astolfo <chdastolfo@gmail.com>
2019-10-13 23:32:44 -0400

    Return early for non dictionary values

--- monday/utils.py
+++ monday/utils.py
@@ -6,6 +6,11 @@ def python_json_stringify(value):
     # ala JSON.stringify, which doesn't quite work the same as json.dumps, unfortunately.
     # According to their API, a proper value for label should look like this:
     # "{\"label\":\"Done\"}"
+
+    # Don't do anything with regular ol' string values
+    if type(value) != dict:
+        return
+
     value = json.dumps(json.dumps(value))
     # Replace double slashes from above with single slash, or just remove them.
     return re.sub('\\\\\\\\', r'\\', value)
