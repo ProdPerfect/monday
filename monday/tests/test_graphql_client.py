8943b99ab99c418f0edce437bf34c3c689ab2b75
Christina D'Astolfo <chdastolfo@gmail.com>
2021-05-30 20:49:56 -0400

    add tests for graphql client

---
+++ monday/tests/test_graphql_client.py
@@ -0,0 +1,18 @@
+import unittest
+from monday.graphqlclient.client import GraphQLClient
+
+
+class GraphQlClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.token = "foo"
+        self.url = "https://api.monday.com/v2'"
+        self.client = GraphQLClient(self.url)
+
+    def test_client_endpoint(self):
+        self.assertEqual(self.client.endpoint, self.url)
+
+    def test_inject_token(self):
+        client = self.client
+        client.inject_token(token=self.token)
+        self.assertEqual(client.token, self.token)
+        self.assertEqual(client.headername, 'Authorization')
