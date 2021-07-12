3a97bf276cf35d153bc31f4c0183a7e6ccc967f6
Christina D'Astolfo <chdastolfo@gmail.com>
2021-07-11 22:39:59 -0400

    methods to properly format columns and nested return arguments

--- monday/query.py
+++ monday/query.py
@@ -3,61 +3,39 @@
 # column values should always been double encoded wherever encountered.
 
 
-class Query:
+class GraphQLQueryBuilder:
     def __init__(self):
-        self.query = {}
-        self.args = ()
-        self.arg_join_string = []
+        self.query_type = ''
+        self.query_args = ()
+        self.query_return_vals = []
         self.quoted_args = ['group_id', 'item_name', 'column_value']
 
-    def format_args(self, args):
-        return ", ".join(["{key}: {value}".format(key=key, value=value) for key, value in args.items()])
+    def format_return_value(self, return_value):
+        values = return_value.split('.')
+        if len(values) == 1:
+            return values[0]
+        else:
+            return "{column} {{ {values} }}".format(column=values.pop(0), values=",".join(values))
 
-    def format_return_values(self, return_values):
-        return ", ".join([arg for arg in return_values])
+    def fields(self, **kwargs):
+        return ", ".join(["{key}: {value}".format(key=key, value=value) for key, value in kwargs.items()])
 
-    def make_query(self, query_type, resource_type, args, return_values):
-        return "{query_type}{{ {resource_type}({args}){{ {kwargs} }}".format(query_type=query_type,
-                                                                             resource_type=resource_type,
-                                                                             args=self.format_args(
-                                                                                 args),
-                                                                             kwargs=self.format_return_values(
-                                                                                 return_values))
+    def return_values(self, return_values):
+        return ", ".join([self.format_return_value(value) for value in return_values])
 
+    def query_type(self, query_type, category):
+        return "{query_type} {{ {category} }}".format(query_type=query_type, category=category)
 
-'''query
-    {
-        boards (ids: %s) {
-            id
-            name
-            permissions
-            tags {
-              id
-              name
-            }
-            groups {
-                id
-                title
-            }
-            columns {
-                id
-                title
-                type
-                settings_str
-            }
-        }
-    }'''
+    def make_query(self, operation, query_type):
+        return "{operation}{{ {query_type}({args}){{ {kwargs} }}".format(operation=operation, query_type=query_type)
 
-'''
-    mutation
-    {
-        create_group(board_id: %s, group_name: "%s")
-        {
-            id
-        }
-    }'''
 
 if __name__ == '__main__':
-    print(Query().make_query('query', 'updates', {'page': 1, 'limit': 25}, ['id', 'body']))
-    print(Query().make_query('mutation', 'create_group', {'board_id': 7, 'group_name': "cheese"}, ['id']))
+    builder = GraphQLQueryBuilder()
+    print(builder.return_values(return_values=['name', 'permissions', 'groups.id.title', 'columns.id.title.type.settings_str']))
+    builder.fields(board_id=7, group_name='cheese')
+    # print(builder.make_query(operation='query', query_type='boards').fields(board_id=7, group_name='cheese').return_values('name', 'permissions', 'groups.id.title', 'columns.id.title.type.settings_str'))
+    # print(builder.make_query('query', 'updates', {'page': 1, 'limit': 25}, ['id', 'body']))
+    # print(builder.make_query('mutation', 'create_group', {'board_id': 7, 'group_name': "cheese"}, ['id']))
+
 
