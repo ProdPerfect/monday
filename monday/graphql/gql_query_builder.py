363630316f844994867cc572686e9ff72a063957
Christina D'Astolfo <chdastolfo@gmail.com>
2021-07-12 23:01:42 -0400

    Adding handling of queries with nested fields and it is perhaps the jankiest thing ever but it works and i will come back and revisit it

--- monday/graphql/gql_query_builder.py
+++ monday/graphql/gql_query_builder.py
@@ -5,6 +5,7 @@ class GraphQLQueryBuilder:
     def __init__(self):
         self.fields = None
         self.return_vals = None
+        self.closing_brackets = ''
         self.quoted_args = ['group_id', 'item_name', 'column_value']
         self.specially_encoded_fields = ['column_values']
         super(GraphQLQueryBuilder, self).__init__()
@@ -31,24 +32,27 @@ class GraphQLQueryBuilder:
         return "{column} {{ {values} }}".format(column=values.pop(0), values=",".join(values)) if len(values) > 1 \
             else values[0]
 
-    def handle_nested_fields(self, nested_fields):
-        return "{key}: ({values})".format(key=nested_fields.keys(), values=nested_fields.values())
-
-    def query_fields(self, **kwargs):
-        # if kwargs.get('nested_fields'):
-        #     self.handle_nested_fields(kwargs.get('nested_fields'))
-        query_field_string = ", ".join(
-            ["{key}: {value}".format(key=key, value=self.encode_values(key, value)) for key, value in kwargs.items()])
-
-        self.fields = query_field_string
+    def query_fields(self, query_type, **kwargs):
+        if self.fields:
+            new_fields = ", ".join(["{key}: {value}".format(key=key, value=self.encode_values(key, value))
+                                    for key, value in kwargs.items()])
+            self.fields = '{existing_fields} {{ {query_type}({new_fields}) '.format(existing_fields=self.fields,
+                                                                                    query_type=query_type,
+                                                                                    new_fields=new_fields)
+            self.closing_brackets += '}'
+        else:
+            query_field_string = ", ".join(
+                ["{key}: {value}".format(key=key, value=self.encode_values(key, value)) for key, value in
+                 kwargs.items()])
+            self.fields = '{query_type}({fields})'.format(query_type=query_type, fields=query_field_string)
         return self
 
     def return_values(self, return_values: list):
         self.return_vals = ", ".join([self.format_return_value(value) for value in return_values])
         return self
 
-    def make_query(self, operation: str, query_type: str) -> str:
-        return "{operation}{{ {query_type}({query_fields}) {{ {return_vals} }} }} ".format(operation=operation,
-                                                                                           query_type=query_type,
-                                                                                           query_fields=self.fields,
-                                                                                           return_vals=self.return_vals)
+    def make_query(self, operation: str) -> str:
+        return "{operation}{{ {query_fields} {{ {return_vals} }} }} {closing_brackets}".format(operation=operation,
+                                                                                               query_fields=self.fields,
+                                                                                               return_vals=self.return_vals,
+                                                                                               closing_brackets=self.closing_brackets)
