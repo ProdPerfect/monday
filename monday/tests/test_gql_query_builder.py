f40bde4c16c6e561ff55ac7a9707df174042a331
Christina D'Astolfo <chdastolfo@gmail.com>
2021-07-13 18:51:18 -0400

    Add test for chained query build

--- monday/tests/test_gql_query_builder.py
+++ monday/tests/test_gql_query_builder.py
@@ -13,13 +13,13 @@ class GraphQLQueryBuilderTestCase(unittest.TestCase):
 
     def test_return_values(self):
         return_value_string = self.query_builder.return_values(['permissions', 'group.id.title']).return_vals
-        self.assertEqual(return_value_string, 'permissions, group {id,title}')
+        self.assertEqual(return_value_string, 'permissions, group{id,title}')
 
     def test_format_return_values(self):
         return_value = self.query_builder.format_return_value('permissions')
         nested_return_value = self.query_builder.format_return_value('group.id.title')
         self.assertEqual(return_value, 'permissions')
-        self.assertEqual(nested_return_value, 'group {id,title}')
+        self.assertEqual(nested_return_value, 'group{id,title}')
 
     def test_encode_values(self):
         should_be_quoted = self.query_builder.encode_values('group_id', 'topics')
@@ -34,3 +34,9 @@ class GraphQLQueryBuilderTestCase(unittest.TestCase):
         self.query_builder.return_vals = 'after,reset_in_x_seconds'
         full_query = self.query_builder.make_query(operation='query')
         self.assertEqual(full_query, 'query{complexity{after,reset_in_x_seconds}}')
+
+    def test_chained_query_build(self):
+        query = self.query_builder.query_fields(query_type='boards', ids=1234567).query_fields(
+            query_type='groups', ids="topics").return_values(['id', 'title', 'items.id.name']).make_query(
+            operation='query')
+        self.assertEqual(query, 'query{boards(ids: 1234567){groups(ids: topics) {id, title, items{id,name}}}}')
