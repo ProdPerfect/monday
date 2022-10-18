f971584e53384f40bfe754189a9633f140ef38df
albcl <a.cerveto@gmail.com>
2022-10-18 17:19:42 +0200

    More custom type hints

--- monday/resources/types.py
+++ monday/resources/types.py
@@ -7,3 +7,19 @@ class BoardKind(Enum):
     PUBLIC = "public"
     PRIVATE = "private"
     SHARE = "share"
+
+
+class BoardState(Enum):
+    """Board available states"""
+
+    ALL = "all"
+    ACTIVE = "active"
+    ARCHIVED = "archived"
+    DELETED = "deleted"
+
+
+class BoardsOrderBy(Enum):
+    """Order to retrieve boards"""
+
+    CREATED_AT = "created_at"
+    USED_AT = "used_at"
