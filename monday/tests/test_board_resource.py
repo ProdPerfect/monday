6325c191460eb14f3bb5cd58987e0b93cd2efff6
Taylor Cochran <tcochran.dev@gmail.com>
2022-12-04 09:53:49 -0800

    Add in no params test

--- monday/tests/test_board_resource.py
+++ monday/tests/test_board_resource.py
@@ -37,10 +37,14 @@ class BoardTestCase(BaseTestCase):
         self.assertIn(str(self.board_id), query)
 
     def test_get_board_items_query(self):
+        query = get_board_items_query(board_id=self.board_id)
+        self.assertIn(str(self.board_id), query)
+        items_line = f'items()'
+        self.assertIn(items_line, query)
+
         limit = 100
         page = 1
         query = get_board_items_query(board_id=self.board_id, limit=limit, page=page)
-        self.assertIn(str(self.board_id), query)
         items_line = f'items(limit: {limit}, page: {page})'
         self.assertIn(items_line, query)
 
