5027df3c05a7f09a0babf4f97e4b8ed7b4b4bdb7
Christina D'Astolfo <chdastolfo@gmail.com>
2022-12-15 14:16:17 -0500

    Fix __email__ typo

--- monday/__version__.py
+++ monday/__version__.py
@@ -1,3 +1,3 @@
 __version__ = '1.3.3'
 __author__ = 'Christina D\'Astolfo'
-__email__ = 'chdastolfo@gmail.com, lemi@prodperfect.com', 'pevner@prodperfect.com'
+__email__ = 'chdastolfo@gmail.com, lemi@prodperfect.com, pevner@prodperfect.com'
